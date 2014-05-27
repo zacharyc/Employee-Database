90ca8f83ce3d764275e3f4795870468311a3de88
Zachary Cohen <noreply>
2014-05-27 16:02:44 +0000

    Add component employee-item

---
+++ ui/employee-item.reel/employee-item.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/employee-item.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class EmployeeItem
+ * @extends Component
+ */
+exports.EmployeeItem = Component.specialize(/** @lends EmployeeItem# */ {
+    constructor: {
+        value: function EmployeeItem() {
+            this.super();
+        }
+    }
+});
